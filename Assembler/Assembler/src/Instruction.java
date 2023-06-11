860bdea50cbb5851948bebf16012a33f4710ac60
Sayed Hossein Hosseini Dolat Abadi <s.hossein.hosseini1381@gmail.com>
2023-06-11 19:04:13 +0330

    Edit the calculation part of type R commands

--- Assembler/Assembler/src/Instruction.java
+++ Assembler/Assembler/src/Instruction.java
@@ -2,19 +2,22 @@ public class Instruction {
     private static String machineCode = "";
 
     private static int main(String instruct) {
+        instruct = "add $t1, $t2, $a1";
         instruct = instruct.trim();
-        String type = R_type.callOrder(instruct.substring(0, 3));
+        String[] type = instruct.split(" ", 2); // Command Type Separation
+        String typeValidation = R_type.callOrder(type[0]); // To help with validation
 
         if (!type.equals(null)) { // R-type
+            String[] register = type[1].split(", ");
             machineCode += "000" +
-                    R_type.callOrder(instruct.substring(9, 12)) + // rs
-                    R_type.callOrder(instruct.substring(14)) + // rt
-                    R_type.callOrder(instruct.substring(4, 7)) + // rd
+                    R_type.callOrder(type[1]) + // rs
+                    R_type.callOrder(type[2]) + // rt
+                    R_type.callOrder(type[0]) + // rd
                     "00000000000000" + // Don't Care
                     type;
 
         } else { // V-type
-
+            machineCode += V_type.callOrder(instruct.substring(0, 3));
         }
         return 0;
     }
