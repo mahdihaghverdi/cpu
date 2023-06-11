3bdc888d928de9d28c3b830e2589d576eea2f6f3
MahdiHaghverdi <mahdihaghverdliewpl@gmail.com>
2023-06-11 20:26:17 +0330

    rewrite the mux to be 8 to 1

--- src/mux4x1.vhdl
+++ src/mux4x1.vhdl
@@ -2,19 +2,30 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity mux4x1 is
+entity mux8x1 is
     port (
-        i0, i1, i2, i3 : in unsigned (7 downto 0);
-        sel : in std_logic_vector (1 downto 0);
+        i0,
+        i1,
+        i2,
+        i3,
+        i4,
+        i5,
+        i6,
+        i7    : in unsigned (7 downto 0);
+        sel   : in std_logic_vector (2 downto 0);
         output: out unsigned (7 downto 0)
     );
-end mux4x1;
+end mux8x1;
 
-architecture arch of mux4x1 is
+architecture arch of mux8x1 is
 begin
     with sel select output <=
-        i0 when "00",
-        i1 when "01",
-        i2 when "10",
-        i3 when others;
+        i0 when "000",
+        i1 when "001",
+        i2 when "010",
+        i3 when "011",
+        i4 when "100",
+        i5 when "101",
+        i6 when "110",
+        i7 when others;
 end arch;
