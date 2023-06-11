b0d65b601163bccf73b38af33813a27cd5d1e0fb
Sayed Hossein Hosseini Dolat Abadi <s.hossein.hosseini1381@gmail.com>
2023-06-11 19:19:25 +0330

    Completing the calculation of the machine code of type R commands

--- Assembler/Assembler/src/Instruction.java
+++ Assembler/Assembler/src/Instruction.java
@@ -1,8 +1,8 @@
 public class Instruction {
     private static String machineCode = "";
 
-    private static int main(String instruct) {
-        instruct = "add $t1, $t2, $a1";
+    public static String main(String instruct) {
+//        instruct = "add $t1, $t2, $a1";
         instruct = instruct.trim();
         String[] type = instruct.split(" ", 2); // Command Type Separation
         String typeValidation = R_type.callOrder(type[0]); // To help with validation
@@ -10,15 +10,15 @@ public class Instruction {
         if (!type.equals(null)) { // R-type
             String[] register = type[1].split(", ");
             machineCode += "000" +
-                    R_type.callOrder(type[1]) + // rs
-                    R_type.callOrder(type[2]) + // rt
-                    R_type.callOrder(type[0]) + // rd
+                    RegisterFile.callRegister(register[1]) + // rs
+                    RegisterFile.callRegister(register[2]) + // rt
+                    RegisterFile.callRegister(register[0]) + // rd
                     "00000000000000" + // Don't Care
-                    type;
+                    typeValidation;
 
         } else { // V-type
             machineCode += V_type.callOrder(instruct.substring(0, 3));
         }
-        return 0;
+        return machineCode;
     }
 }
